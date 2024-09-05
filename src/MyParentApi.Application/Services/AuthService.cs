da92d9de8796cc693b9cfb6700dea3b40531914c
Leonardo Ribeiro <55027647+leorbmello@users.noreply.github.com>
2024-09-05 13:28:50 -0300

    up

--- src/MyParentApi.Application/Services/AuthService.cs
+++ src/MyParentApi.Application/Services/AuthService.cs
@@ -1,6 +1,8 @@
-﻿using MyParentApi.Application.DTOs.Requests;
+﻿using Microsoft.Extensions.Logging;
+using MyParentApi.Application.DTOs.Requests;
 using MyParentApi.Application.DTOs.Responses;
 using MyParentApi.Application.Interfaces;
+using MyParentApi.DAL;
 
 namespace MyParentApi.Application.Services
 {
@@ -8,22 +10,44 @@ namespace MyParentApi.Application.Services
     {
         private readonly ITokenService tokenService;
         private readonly AppDbContext context;
-
-        public AuthService(ITokenService tokenService, AppDbContext context)
+        private readonly ILogger<AuthService> logger;
+        public AuthService(
+            ILogger<AuthService> logger,
+            ITokenService tokenService, 
+            AppDbContext context)
         {
+            this.logger = logger;
             this.tokenService = tokenService;
             this.context = context;
         }
 
         public AuthResponse Authenticate(AuthRequest request)
         {
-            var user = context.Users.FirstOrDefault(x => x.Email.Equals(request.Email));
-            if (user == null || !ValidatePassword(request.Password, user.PasswordHash))
+            try
             {
+                var user = context.Users.FirstOrDefault(x => x.Email.Equals(request.Email));
+                if (user == null || !ValidatePassword(request.Password, user.PasswordHash))
+                {
+                    return new AuthResponse(SystemErrorCode_InvalidCredentials, null);
+                }
+
+                return new AuthResponse(SystemErrorCode_LoginOk, tokenService.GenerateToken(user.Email));
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex.ToString());
                 return new AuthResponse(SystemErrorCode_InvalidCredentials, null);
             }
+        }
 
-            return new AuthResponse(SystemErrorCode_LoginOk, tokenService.GenerateToken(user.Email));
+        public void Logout(AuthRefreshRequest request)
+        {
+            if (string.IsNullOrEmpty(request.Token))
+            {
+                return;
+            }
+
+            AddToBlackList(request.Token);
         }
 
         public bool ValidatePassword(string password, string passwordHash)
