ff920d1b3a03cfda20b629d088ff017ca2151b77
Leonardo Ribeiro <55027647+leorbmello@users.noreply.github.com>
2024-09-29 17:19:02 -0300

    Ajuste na ProfileService
    Ajuste na ProfileService

--- src/MyParentApi.Application/Services/ProfileService.cs
+++ src/MyParentApi.Application/Services/ProfileService.cs
@@ -9,116 +9,97 @@ namespace MyParentApi.Application.Services
 {
     public class ProfileService : IProfileService
     {
-        private readonly ILogger<ProfileService> logger;
 
         private readonly ISysLogService sysLogService;
         private readonly IUserRepository userRepository;
 
         public ProfileService(
-            ILogger<ProfileService> logger,
             ISysLogService sysLogService,
             IUserRepository userRepository,
             AppDbContext context) 
         {
-            this.logger = logger;
             this.userRepository = userRepository;
             this.sysLogService = sysLogService;
         }
 
         public async Task<GenericResponse> ChangePasswordAsync(PasswordChangeRequest request)
         {
-            try
+            var user = await userRepository.GetUserAsync(request.Email);
+            if (user == null)
             {
-                var user = await userRepository.GetUserAsync(request.Email);
-                if (user == null)
-                {
-                    return new GenericResponse(StrTitleError, StrAuthInvalid);
-                }
-
-                var result = WhirlPoolHashService.CheckPassword(request.OldPassword, user.PasswordHash, user.Salt);
-                if (result != Microsoft.AspNetCore.Identity.PasswordVerificationResult.Success)
-                {
-                    return new GenericResponse(StrTitleError, StrUserPasswordNotMatch);
-                }
-
-                if (!WhirlPoolHashService.IsValidPassword(request.NewPassword))
-                {
-                    return new GenericResponse(StrTitleError, StrPasswordSecurityLow);
-                }
-
-                if (!request.NewPassword.Equals(request.NewPasswordConfirm))
-                {
-                    return new GenericResponse(StrTitleError, StrPasswordChangeNotMatch);
-                }
-
-                var newSalt = WhirlPoolHashService.GenerateSalt();
-                var newPass = WhirlPoolHashService.HashPassword(request.NewPassword, newSalt);
-                user.PasswordHash = newPass;
-                user.Salt = newSalt;
-                if (await userRepository.UpdateUserAsync(user))
-                {
-                    await sysLogService.SaveUserLogAsync(user.Id, StrOperationPasswordChange, JsonSerializer.Serialize(user));
-                    return new GenericResponse(StrTitleAdvertise, StrPasswordChangeSuccess);
-                }
-            }
-            catch (Exception ex)
-            {
-                logger.LogError(ex.ToString());
-                throw new ProfileException(GetType().Name, ex);
+                throw new ProfileException(GetType().Name, StrAuthInvalid);
             }
 
-            return new GenericResponse(StrTitleError, StrPasswordChangeError);
+            var result = WhirlPoolHashService.CheckPassword(request.OldPassword, user.PasswordHash, user.Salt);
+            if (result != Microsoft.AspNetCore.Identity.PasswordVerificationResult.Success)
+            {
+                throw new ProfileException(GetType().Name, StrUserPasswordNotMatch);
+            }
+
+            if (!WhirlPoolHashService.IsValidPassword(request.NewPassword))
+            {
+                throw new ProfileException(GetType().Name, StrPasswordSecurityLow);
+            }
+
+            if (!request.NewPassword.Equals(request.NewPasswordConfirm))
+            {
+                throw new ProfileException(GetType().Name, StrPasswordChangeNotMatch);
+            }
+
+            var newSalt = WhirlPoolHashService.GenerateSalt();
+            var newPass = WhirlPoolHashService.HashPassword(request.NewPassword, newSalt);
+            user.PasswordHash = newPass;
+            user.Salt = newSalt;
+            if (await userRepository.UpdateUserAsync(user))
+            {
+                await sysLogService.SaveUserLogAsync(user.Id, StrOperationPasswordChange, JsonSerializer.Serialize(user));
+                return new GenericResponse(StrTitleAdvertise, StrPasswordChangeSuccess);
+            }
+
+            throw new ProfileException(GetType().Name, StrPasswordChangeError);
         }
 
         public async Task<GenericResponse> ChangePasswordByTokenAsync(PasswordChangeRequest request)
         {
-            try
+            var user = await userRepository.GetUserAsync(request.Email);
+            if (user == null)
             {
-                var user = await userRepository.GetUserAsync(request.Email);
-                if (user == null)
-                {
-                    return new GenericResponse(StrTitleError, StrAuthInvalid);
-                }
-
-                var recoveryRequest = await userRepository.GetRecoveryKeyAsync(request.Email);
-                if (recoveryRequest == null)
-                {
-                    return new GenericResponse(StrTitleAdvertise, StrRecoveryInvalid);
-                }
-
-                if (!recoveryRequest.Equals(request.OldPassword))
-                {
-                    return new GenericResponse(StrTitleAdvertise, StrRecoveryInvalidToken);
-                }
-
-                if (!WhirlPoolHashService.IsValidPassword(request.NewPassword))
-                {
-                    return new GenericResponse(StrTitleError, StrPasswordSecurityLow);
-                }
-
-                if (!request.NewPassword.Equals(request.NewPasswordConfirm))
-                {
-                    return new GenericResponse(StrTitleError, StrPasswordChangeNotMatch);
-                }
-
-                var newSalt = WhirlPoolHashService.GenerateSalt();
-                var newPass = WhirlPoolHashService.HashPassword(request.NewPassword, newSalt);
-                user.PasswordHash = newPass;
-                user.Salt = newSalt;
-                if (await userRepository.UpdateUserAsync(user))
-                {
-                    await userRepository.DeletePassRecoveryAsync(recoveryRequest);
-                    await sysLogService.SaveUserLogAsync(user.Id, StrOperationPasswordChange, JsonSerializer.Serialize(user));
-                    return new GenericResponse(StrTitleAdvertise, StrPasswordChangeSuccess);
-                }
-            }
-            catch (Exception ex)
-            {
-                logger.LogError(ex.ToString());
-                throw new ProfileException(GetType().Name, ex);
+                throw new ProfileException(GetType().Name, StrAuthInvalid);
             }
 
-            return new GenericResponse(StrTitleError, StrPasswordChangeError);
+            var recoveryRequest = await userRepository.GetRecoveryKeyAsync(request.Email);
+            if (recoveryRequest == null)
+            {
+                throw new ProfileException(GetType().Name, StrRecoveryInvalid);
+            }
+
+            if (!recoveryRequest.Equals(request.OldPassword))
+            {
+                throw new ProfileException(GetType().Name, StrRecoveryInvalidToken);
+            }
+
+            if (!WhirlPoolHashService.IsValidPassword(request.NewPassword))
+            {
+                throw new ProfileException(GetType().Name, StrPasswordSecurityLow);
+            }
+
+            if (!request.NewPassword.Equals(request.NewPasswordConfirm))
+            {
+                throw new ProfileException(GetType().Name, StrPasswordChangeNotMatch);
+            }
+
+            var newSalt = WhirlPoolHashService.GenerateSalt();
+            var newPass = WhirlPoolHashService.HashPassword(request.NewPassword, newSalt);
+            user.PasswordHash = newPass;
+            user.Salt = newSalt;
+            if (await userRepository.UpdateUserAsync(user))
+            {
+                await userRepository.DeletePassRecoveryAsync(recoveryRequest);
+                await sysLogService.SaveUserLogAsync(user.Id, StrOperationPasswordChange, JsonSerializer.Serialize(user));
+                return new GenericResponse(StrTitleAdvertise, StrPasswordChangeSuccess);
+            }
+
+            throw new ProfileException(GetType().Name, StrPasswordChangeError);
         }
     }
 }
